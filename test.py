91fbaacef48f957b4e2f995e164276c967d2705d
Roger Esteve <rogeresteve30@gmail.com>
2022-01-10 00:50:05 +0100

    standing history?

--- test.py
+++ test.py
@@ -27,4 +27,63 @@ def request_and_save_match_day(season, league, md):
     f.write(json.dumps(res))
     f.close()
 
-    request_and_save_match_day(2021, "pd", 19)
+def load_stored_match_day(season, league, md):
+    try:
+        f = open("store/" + league + "/" + str(season) +
+                "/md_" + str(md) + ".json", "r")
+        return json.loads(f.read())
+    except OSError:
+        return None
+
+def generate_standing_history(season, league):
+    res = []
+    i = 1
+    while True:
+        data = {}
+        matchday = load_stored_match_day(season, league, i)
+        if matchday == None:
+            break
+        for match in matchday['matches']:
+            t_home = 1
+            t_away = 1
+            if match['score']['winner'] == 'HOME_TEAM':
+                t_home = 3
+                t_away = 0
+            elif match['score']['winner'] == 'AWAY_TEAM':
+                t_home = 0
+                t_away = 3
+            
+            g_home = match['score']['fullTime']['homeTeam']
+            g_away = match['score']['fullTime']['awayTeam']
+
+            if i == 1:
+                data[match['homeTeam']['id']] = {'pts': t_home, 'gd': g_home-g_away}
+                data[match['awayTeam']['id']] = {'pts': t_away, 'gd': g_away-g_home}
+            else:
+                print(i)
+                print(res[i-2])
+                curr_home_pts = res[i-2][match['homeTeam']['id']]['pts']
+                curr_away_pts = res[i-2][match['awayTeam']['id']]['pts']
+                curr_home_gd = res[i-2][match['homeTeam']['id']]['gd']
+                curr_away_gd = res[i-2][match['awayTeam']['id']]['gd']
+                
+                data[match['homeTeam']['id']] = {
+                    'pts': curr_home_pts + t_home,
+                    'gd': curr_home_gd + g_home-g_away
+                }
+
+                data[match['awayTeam']['id']] = {
+                    'pts': curr_away_pts + t_away,
+                    'gd': curr_away_gd + g_away-g_home
+                }
+            
+        i += 1
+        res.append(data)
+    f = open("store/" + league + "/" + str(season) + "/pts_history.json", "w")
+    f.write(json.dumps(res))
+    f.close()
+    return res
+
+
+print(generate_standing_history(2021, 'pd'))
+
